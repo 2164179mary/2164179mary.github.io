221636b3bbea255495bee7d073aa698554fb62cd
Mary <Mary@gmail.com>
2018-04-11 13:24:47 +0800

    pwa cashfe

--- scripts/indexedDB.js
+++ scripts/indexedDB.js
@@ -7,6 +7,7 @@ var menuList = [];
 var cafeList = [];
 
 function getBudget(){
+    budget = 0;
     budget = document.getElementById("budget-value").value;
     modal.style.display = "block";
     console.log(budget);
@@ -19,6 +20,7 @@ window.onclick = function(event) {
 }
 
 function getCategory(id) {
+    category = [];
     var checkBox = document.getElementById(id);
     if(checkBox.checked == true){
         categories.push(id);
@@ -31,6 +33,7 @@ function getCategory(id) {
 }
 
 function getAmbience(id) {
+    ambience = [];
     var checkBox = document.getElementById(id);
     if(checkBox.checked == true){
         ambience.push(id);
@@ -43,6 +46,7 @@ function getAmbience(id) {
 }
 
 function getAvailability(id) {
+    availability = "";
     cafeList = [];
     availability = document.getElementById(id).value;
     console.log(availability);
@@ -64,90 +68,51 @@ const cafe = [
 ];
 
 const menu = [
-    {id: 1, cafeId: 1, menuName: "Brewed Coffee", menuType: "coffee", price: 59},
-    {id: 2, cafeId: 1, menuName: "Espresso Con Panna", menuType: "coffee", price: 59},
-    {id: 3, cafeId: 1, menuName: "Green Tea Latte", menuType: "coffee", price: 89},
-    {id: 4, cafeId: 1, menuName: "Caramel Macchiato", menuType: "coffee", price: 85},
-    {id: 5, cafeId: 1, menuName: "Mocha Latte", menuType: "coffee", price: 85},
-    {id: 6, cafeId: 1, menuName: "Cappucino", menuType: "coffee", price: 79},
-    {id: 7, cafeId: 1, menuName: "Coffe Latte", menuType: "coffee", price: 85},
-    {id: 8, cafeId: 1, menuName: "Espresso Hot Chocolate", menuType: "coffee", price: 69},
-    {id: 9, cafeId: 1, menuName: "Americano", menuType: "coffee", price: 59},
-    {id: 10, cafeId: 1, menuName: "Hot Milk", menuType: "coffee", price: 59},
-    {id: 11, cafeId: 1, menuName: "Pasta Classic", menuType: "pasta", price: 129},
-    {id: 12, cafeId: 1, menuName: "Carbonara", menuType: "pasta", price: 129},
-    {id: 13, cafeId: 1, menuName: "Tuna Garlic Pasta", menuType: "pasta", price: 129},
-    {id: 14, cafeId: 1, menuName: "Yummy Cheezy Lasagna", menuType: "pasta", price: 129},
-    {id: 15, cafeId: 1, menuName: "Chicken Strips, toasted bread", menuType: "pasta", price: 149},
-    {id: 16, cafeId: 1, menuName: "Four Seasons", menuType: "juice", price: 55},
-    {id: 17, cafeId: 1, menuName: "Lemon Iced Tea", menuType: "juice", price: 55},
-    {id: 18, cafeId: 1, menuName: "Cranberry Iced Tea", menuType: "juice", price: 55},
-    {id: 19, cafeId: 1, menuName: "Cranberry Iced Tea Pitcher", menuType: "juice", price: 120},
-    {id: 20, cafeId: 1, menuName: "Mango", menuType: "juice", price: 55},
-    {id: 21, cafeId: 1, menuName: "Pinapple", menuType: "juice", price: 55},
-    {id: 22, cafeId: 1, menuName: "Coke", menuType: "juice", price: 45},
-    {id: 23, cafeId: 1, menuName: "Bottled Water", menuType: "juice", price: 30},
-    {id: 24, cafeId: 1, menuName: "Java Chip", menuType: "frappe", price: 129},
-    {id: 25, cafeId: 1, menuName: "Mango Graham Frappe", menuType: "frappe", price: 129},
-    {id: 26, cafeId: 1, menuName: "Strawberry Frappe", menuType: "frappe", price: 129},
-    {id: 27, cafeId: 1, menuName: "Nutty Hazelnut Frappe", menuType: "frappe", price: 149},
-    {id: 28, cafeId: 1, menuName: "Cookies and Cream Frappe", menuType: "frappe", price: 109},
-    {id: 29, cafeId: 1, menuName: "Ube Milkslush Frappe", menuType: "frappe", price: 109},
-    {id: 30, cafeId: 1, menuName: "Pandan Frappe", menuType: "frappe", price: 129},
-    {id: 31, cafeId: 1, menuName: "Bubble Gum Frappe", menuType: "frappe", price: 129},
-    {id: 32, cafeId: 1, menuName: "Wintermelon Milk Tea", menuType: "milk tea", price: 79},
-  	{id: 33, cafeId: 1, menuName: "Thai-Style Milk Tea", menuType: "milk tea", price: 79},
-  	{id: 34, cafeId: 1, menuName: "Hokkaido Style Milk Tea", menuType: "milk tea", price: 79},
-  	{id: 35, cafeId: 2, menuName: "Secret Garden's Beef Tapa", menuType: "rice meal", price: 195},
-  	{id: 36, cafeId: 2, menuName: "Beef Belly Roast", menuType: "rice meal", price: 180},
-  	{id: 37, cafeId: 2, menuName: "Beef Broccoli", menuType: "rice meal", price: 180},
-  	{id: 38, cafeId: 2, menuName: "Beef Salpicao", menuType: "rice meal", price: 195},
-  	{id: 39, cafeId: 2, menuName: "Pork Liempo", menuType: "rice meal", price: 165},
-  	{id: 40, cafeId: 2, menuName: "Pork Ribs", menuType: "rice meal", price: 180},
-  	{id: 41, cafeId: 2, menuName: "Pork Lechon Kawali", menuType: "rice meal", price: 250},
-  	{id: 42, cafeId: 2, menuName: "Cream Garlic Chicken", menuType: "rice meal", price: 230},
-  	{id: 43, cafeId: 2, menuName: "Sween and Sour Fish Fillet", menuType: "rice meal", price: 175},
-  	{id: 44, cafeId: 2, menuName: "Steamed Fish", menuType: "rice meal", price: 220},
-  	{id: 45, cafeId: 2, menuName: "Beef steak", menuType: "rice meal", price: 450},
-  	{id: 46, cafeId: 2, menuName: "Bolognese", menuType: "pasta", price: 195},
-  	{id: 47, cafeId: 2, menuName: "Seafood Pasta", menuType: "pasta", price: 290},
-  	{id: 48, cafeId: 2, menuName: "Shirmp Chili Pasta", menuType: "pasta", price: 260},
-  	{id: 49, cafeId: 2, menuName: "Garlic Pasta", menuType: "pasta", price: 170},
-  	{id: 50, cafeId: 2, menuName: "Carbonara", menuType: "pasta", price: 250},
-  	{id: 51, cafeId: 2, menuName: "Pesto Pasta", menuType: "pasta", price: 175},
-  	{id: 52, cafeId: 2, menuName: "Creamy Pesto Pasta", menuType: "pasta", price: 235},
-  	{id: 53, cafeId: 2, menuName: "Vongole in Olive Oil", menuType: "pasta", price: 185},
-  	{id: 54, cafeId: 2, menuName: "Vongole in Tomato", menuType: "pasta", price: 220},
-  	{id: 55, cafeId: 2, menuName: "Pomodoro", menuType: "pasta", price: 180},
-  	{id: 56, cafeId: 2, menuName: "Fusilli Chilli", menuType: "pasta", price: 240},
-	{id: 57, cafeId: 3, menuName: "Brown Cookies", menuType: "pasta", price: 100},
-	{id: 58, cafeId: 3, menuName: "Vanilla tea", menuType: "milk", price: 120},
-    {id: 59, cafeId: 3, menuName: "milk Frappe", menuType: "frappe", price: 320},
-    {id: 60, cafeId: 3, menuName: "milk juice", menuType: "juice", price: 786},
-    {id: 61, cafeId: 3, menuName: "Hotdog mean", menuType: "rice", price: 111},
+    {id: 1, cafeId: 1, menuName: "Coffee", menuType: "coffee", price: 120},
+    {id: 2, cafeId: 1, menuName: "Strawberry Shortcake", menuType: "cakes", price: 536},
+    {id: 3, cafeId: 1, menuName: "Chocolate Cookies", menuType: "cookies", price: 154},
+    {id: 4, cafeId: 1, menuName: "Vanilla Frappe", menuType: "frappe", price: 120},
+    {id: 5, cafeId: 1, menuName: "Orange Juice", menuType: "juice", price: 50},
+    {id: 6, cafeId: 1, menuName: "Taro", menuType: "Milk tea", price: 300},
+    {id: 7, cafeId: 1, menuName: "Carbonara", menuType: "pasta", price: 400},
+    {id: 8, cafeId: 1, menuName: "Meat and Fist", menuType: "rice", price: 520},
+    {id: 9, cafeId: 2, menuName: "Fish", menuType: "rice", price: 220},
+    {id: 10, cafeId: 2, menuName: "Matcha Cake", menuType: "cakes", price: 600},
+    {id: 11, cafeId: 2, menuName: "Matcha Frappe", menuType: "frappe", price: 400},
+    {id: 12, cafeId: 2, menuName: "Matcha Juice", menuType: "juice", price: 50},
+    {id: 13, cafeId: 2, menuName: "Matcha Tea", menuType: "milk", price: 123},
+    {id: 14, cafeId: 2, menuName: "Matcha Pasta", menuType: "pasta", price: 430},
+    {id: 15, cafeId: 3, menuName: "milk Cake", menuType: "cakes", price: 320},
+    {id: 16, cafeId: 3, menuName: "milk Cookies", menuType: "cookies", price: 500},
+    {id: 17, cafeId: 3, menuName: "Sweet pasta", menuType: "pasta", price: 120},
+    {id: 18, cafeId: 3, menuName: "vanilla latte", menuType: "coffee", price: 120},
+    {id: 19, cafeId: 3, menuName: "Vanilla tea", menuType: "milk", price: 120},
+    {id: 20, cafeId: 3, menuName: "milk Frappe", menuType: "frappe", price: 320},
+    {id: 21, cafeId: 3, menuName: "milk juice", menuType: "juice", price: 786},
+    {id: 22, cafeId: 3, menuName: "Hotdog mean", menuType: "rice", price: 111},
 ]
  
  
-var db;
-var request = window.indexedDB.open("newdbase", 2);
+var dbb;
+var request = window.indexedDB.open("dbasevone", 1);
  
 request.onerror = function(event) {
   console.log("error: ");
 };
  
 request.onsuccess = function(event) {
-  db = request.result;
-  console.log("success: "+ db);
+  dbb = request.result;
+  console.log("success: "+ dbb);
 };
  
 request.onupgradeneeded = function(event) {
-        var db = event.target.result;
-        var objectStore = db.createObjectStore("cafe", {keyPath: "id"});
+        var dbb = event.target.result;
+        var objectStore = dbb.createObjectStore("cafe", {keyPath: "id"});
         for (var i in cafe) {
                 objectStore.add(cafe[i]);      
         }
     
-        var objectStore2 = db.createObjectStore("menu", {keyPath: "id"});
+        var objectStore2 = dbb.createObjectStore("menu", {keyPath: "id"});
         for (var i in menu) {
                 objectStore2.add(menu[i]);      
         }
@@ -162,7 +127,7 @@ function compile(){
 var printCafe = function(){
     var iterate = 0;
     var j = 0;
-    var objectStore = db.transaction("cafe").objectStore("cafe");
+    var objectStore = dbb.transaction("cafe").objectStore("cafe");
         objectStore.openCursor().onsuccess = function(event) {
           var cursor = event.target.result;
           if (cursor) {
@@ -213,6 +178,30 @@ var printCafe = function(){
                     }
                     }
                   }else{
+                      if(categories.length == 0 && ambience.length == 0){
+                          console.log("You");
+                          if(cursor.value.wifi == availability){ 
+                              var print = cursor.value.cafeName;
+                                    var location = cursor.value.cafeLocation;
+                                    var id = cursor.value.id;
+                                    var ids = "a"+id+"b";
+                                    var prints = print+"a";
+                                    var div = document.createElement('div');
+                                    div.setAttribute('id', ids);
+                                    var a = document.createElement('button');
+                                    //a.setAttribute('href', "");
+                                    a.setAttribute('id', id);
+                                    a.setAttribute('value', iterate);
+                                    a.setAttribute('onclick', "callPrintMenu(this.id, this.getAttribute('value'))");
+                                    a.innerHTML = print+"\t\t Location: "+location;
+                                    var li = document.createElement('li');
+                                      //li.setAttribute('id', i);
+                                    var element = document.getElementById("cafeResults");
+                                    element.appendChild(li);
+                                    li.appendChild(a);
+                                    li.appendChild(div);
+                          }
+                      }else 
                     for(var i=0; i < ambience.length; i++){
                     if(cursor.value.wifi == availability){ 
                         var index = 0;
@@ -279,7 +268,7 @@ var printCafe = function(){
 
 var printMenu = function(id){
     var ids ="a"+id+"b";
-    var objectStore = db.transaction("menu").objectStore("menu");
+    var objectStore = dbb.transaction("menu").objectStore("menu");
   
         objectStore.openCursor().onsuccess = function(event) {
           var cursor = event.target.result;
@@ -346,6 +335,14 @@ var callPrintMenu = function(id, index){
 var resetCafe = function(){
    location.reload();
 
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+}else{
+    console.log("ERROR");
 }
 
 
