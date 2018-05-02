ebc2d5c57dfb0924d47f64cbf2d995ece9c18b07
Mary Ann Licudo <34298410+2164179mary@users.noreply.github.com>
2018-05-02 15:37:41 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,26 @@ const staticAssets = [
 	'',
 	'./script/cafe.json',
 	'./script/app.js',
-	'./script/indexeddb.js'
+	'./script/indexeddb.js',
+	'./CSS/fonts/Quesha.ttf',
+	'./CSS/index.css',
+	'./cafeImages/arca.png',
+    './cafeImages/beanstalk_logo.png',
+    './cafeImages/Cafe-in-the-Sky-Baguio-Main.png',
+    './cafeImages/gossip.jpg',
+    './cafeImages/logo.png',
+    './cafeImages/sabel.jpg',
+    './icons/cashfe-72x72.png',
+    './icons/cashfe-96x96.png',
+    './icons/cashfe-128x128.png',
+    './icons/cashfe-144x144.png',
+    './icons/cashfe-152x152.png',
+    './icons/cashfe-192x192.png',
+    './icons/cashfe-384x384.png',
+    './icons/cashfe-512x512.png',
+    'manifest.json',
+    'sw.js'
+
 ];
 self.addEventListener('install', async event => {
     const cache = await caches.open('index-static');
